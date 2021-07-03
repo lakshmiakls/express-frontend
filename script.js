f7edf13357cdd11dc5652a35c440befc7307cfe0
lakshmiakls <lakshmiakls25@gmail.com>
2021-07-03 12:47:41 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:3000/api/posts";
-const BASE_URL = "http://localhost:3000/";
+const API_URL = "https://expresstrial-bylakshmi.herokuapp.com//api/posts";
+
 
 
 window.onload = () =>{
